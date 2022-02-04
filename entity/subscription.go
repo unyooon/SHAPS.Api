219a39438e63cfee6faafc73feeb413d7b8abbab
yuta720 <mryy720@yahoo.co.jp>
2022-02-04 22:44:49 +0900

    ✨ entity追加

---
+++ entity/subscription.go
@@ -0,0 +1,11 @@
+package entity
+
+import "github.com/jinzhu/gorm"
+
+type Subscription struct {
+	gorm.Model
+	Name        string `column:"name"`
+	Price       int    `column:"price"`
+	Term        uint   `column:"term"`
+	Description string `column:"description"`
+}
