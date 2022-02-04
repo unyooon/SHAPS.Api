335b5b9f087ef4bab783331f459d5f3d579ec251
yuta720 <mryy720@yahoo.co.jp>
2022-02-04 22:45:11 +0900

    🩹 型修正

--- domain/dto/create_subscription_request.go
+++ domain/dto/create_subscription_request.go
@@ -1,7 +1,7 @@
 package dto
 
 type CreateSubscriptionRequest struct {
-	Name        string `column:""`
+	Name        string `column:"name"`
 	Price       int    `column:"price"`
 	Term        uint   `column:"term"`
 	Description string `column:"description"`
