48a5cb86e6a13e7e37340b35d6d4b32b78121633
yuta720 <mryy720@yahoo.co.jp>
2022-02-05 18:56:43 +0900

    ✨ 環境変数読み込み処理追加

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "os"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+
+	// 環境変数読み込み
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	r := InitializeHandler(os.Getenv("DSN"))
 	r.Run()
 }
