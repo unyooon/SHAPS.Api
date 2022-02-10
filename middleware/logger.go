206f36046f31fb15a6f9d0bc2e84c54c19538742
yuta720 <mryy720@yahoo.co.jp>
2022-02-11 00:10:20 +0900

    ✨ logにtraceId追加

--- middleware/logger.go
+++ middleware/logger.go
@@ -5,10 +5,16 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"go.uber.org/zap"
 )
 
 func Logging(c *gin.Context) {
+	traceId, err := uuid.NewRandom()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
 	// request log
 	logger, err := zap.NewProduction()
 	if err != nil {
@@ -17,6 +23,7 @@ func Logging(c *gin.Context) {
 
 	logger.Info(
 		"request",
+		zap.String("traceId", traceId.String()),
 		zap.String("path", c.Request.URL.Path),
 		zap.String("query", c.Request.URL.RawQuery),
 		zap.String("ip", c.ClientIP()),
@@ -28,6 +35,7 @@ func Logging(c *gin.Context) {
 	// response log
 	logger.Info(
 		"response",
+		zap.String("traceId", traceId.String()),
 		zap.String("statusCode", strconv.Itoa(c.Writer.Status())),
 	)
 }
