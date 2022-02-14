84b6af53324661a57060a89e2ce1b46f30581cd2
yuta720 <mryy720@yahoo.co.jp>
2022-02-15 01:15:06 +0900

    ✨ stripe client追加

---
+++ infrastructure/external/exstripe/stripe_client.go
@@ -0,0 +1,17 @@
+package exstripe
+
+import (
+	"github.com/stripe/stripe-go/v72/client"
+)
+
+type Client struct {
+	*client.API
+}
+
+func NewStripeClietn(sk string) *Client {
+	sc := &client.API{}
+	sc.Init(sk, nil)
+	return &Client{
+		sc,
+	}
+}
